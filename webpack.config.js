4f880ee2b5bfeecebe666b038bb8c3797309ea13
Albert Yim <62459059+albert-yim@users.noreply.github.com>
2024-01-13 23:34:15 +0900

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+ 
+module.exports = {
+  mode: 'development',
+  devServer: {
+    contentBase: './dist',
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: {
+      keep: /\.git/,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ]
+}
+출처: https://leettle.tistory.com/4 [leettle log:티스토리]
