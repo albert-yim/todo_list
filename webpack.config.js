0dd03e6b1d22534a2d3dc9a5f48bc6183bb4caa0
albert.yim <albert.yim719@gmail.com>
2024-01-17 23:28:54 +0900

    Fix the bug which is cannot load the file in Web

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const isProduction = process.env.NODE_ENV == "production";
+// const isProduction = process.env.NODE_ENV == "production";
+const isProduction = false;
 
 const stylesHandler = "style-loader";
 
@@ -13,13 +14,11 @@ const config = {
     path: path.resolve(__dirname, "dist"),
   },
   devServer: {
-    // proxy: {
-    //   "/api": {
-    //     target: "http://localhost:8080",
-    //     changeOrigin: true,
-    //   },
-    // },
     allowedHosts: "all",
+    // For resolving error which is 404 get file, (Need to set static directory)
+    static: {
+      directory: path.join(__dirname, "./"),
+    },
     open: false,
     hot: true,
     // historyApiFallback: true,
